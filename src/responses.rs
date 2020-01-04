65ecb146b7435a640dc30b571a7c8a07f3095bcf
Fabian Vowie <crymaprivat@gmail.com>
2020-01-05 00:40:05 +0100

    Add response with pagination and total

--- src/responses.rs
+++ src/responses.rs
@@ -1,6 +1,20 @@
+use std::fmt::Debug;
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct HelixResponse<T: super::traits::TwitchModel> {
     pub data: Vec<T>
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct HelixPaginatedResponse<T: super::traits::TwitchModel> {
+    pub data: Vec<T>,
+    pub pagination: super::models::TwitchPagination
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct HelixPaginatedResponseWithTotal<T: super::traits::TwitchModel> {
+    pub data: Vec<T>,
+    pub pagination: super::models::TwitchPagination,
+    pub total: i32
 }
